c95ae63e0f706d209add830bdd26428507cd3496
TheProgrammerOfLife <IvanChen07@gmail.com>
2021-09-19 21:18:42 -0400

    Fixed next/Link error

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   reactStrictMode: true,
-  assetPrefix: '/blog/',
+  assetPrefix: '/',
   target: 'serverless',
   webpack: function (config) {
     config.module.rules.push({test:  /\.md$/, use: 'raw-loader'})
